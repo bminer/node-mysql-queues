4b0554ff3ca345e720445cea5131a153398f2fa2
Blake Miner <miner.blake@gmail.com>
2011-12-07 22:49:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,87 @@
+var currentlyExecutingQueue = null;
+var mainQueue = [];
+/* Wraps db.query and exposes function db.createQueue() */
+module.exports = function(db) {
+	var dbQuery = db.query; //The old db.query function
+	//Wrap db.query
+	db.query = function(sql, params, cb) {
+		//Run query if no Queue is running; otherwise, queue it in mainQueue
+		if(currentlyExecutingQueue == null)
+			dbQuery.apply(db, arguments);
+		else
+			mainQueue.push(arguments);
+	}
+	//Create a new executable query Queue
+	db.createQueue = function() {
+		return new Queue(dbQuery, function () {
+			//Called when a Queue has completed its processing and main queue should be executed
+			currentlyExecutingQueue = null;
+			while(mainQueue.length > 0)
+			{
+				var item = mainQueue.shift(); //Unsure of shift's performance
+				if(item instanceof Queue)
+				{
+					item.execute();
+					break; //After the Queue has been executed, the main queue will be resumed
+				}
+				else
+					dbQuery.apply(db, item);
+			}
+		});
+	}
+}
+function Queue(dbQuery, resumeMainQueue) {
+	this.queue = [];
+	/* Add a query to the Queue */
+	this.query = function(sql, params, cb) {
+		if(typeof params == "function")
+		{
+			cb = params;
+			params = undefined;
+		}
+		this.queue.push({
+			'sql': sql,
+			'params': params,
+			'cb': cb
+		});
+	};
+	/* Execute all queries on the Queue in order and prevent other queries from executing until
+		all queries have been completed.
+	*/
+	this.execute = function() {
+		var that = this;
+		//If another Queue is currently running, we put this on the mainQueue
+		if(currentlyExecutingQueue != null && currentlyExecutingQueue != this)
+			mainQueue.push(this);
+		else if(that.queue.length > 0)
+		{
+			currentlyExecutingQueue = this;
+			//console.log("Executing queue:", currentlyExecutingQueue);
+			//Run everything in the queue
+			var done = 0, total = that.queue.length;
+			for(var i in that.queue)
+			{
+				(function(item) {
+					//Execute the query
+					dbQuery(item.sql, item.input || [], function() {
+						if(item.cb != null)
+							item.cb.apply(this, arguments);
+						//When the entire queue has completed...
+						if(++done == total)
+						{
+							/* The query's callback may have queued more queries on this Queue.
+								If so, execute this Queue again; otherwise, resumeMainQueue() */
+							if(that.queue.length == 0)
+								resumeMainQueue();
+							else
+								that.execute();
+						}
+					});
+				})(that.queue[i]);
+			}
+			that.queue = [];
+			//All queued queries are running, but we don't resume the main queue just yet
+			//console.log("Queue Complete:", currentlyExecutingQueue);
+		}
+	};
+}
---
+++ tests/test.js
@@ -0,0 +1,66 @@
+var queue = require('../server/lib/mysql-query-queue');
+var db = {
+	'query': function(sql, input, cb) {
+		if(typeof input == "function")
+		{
+			cb = input;
+			input = undefined;
+		}
+		console.log("Executing " + sql);
+		setTimeout(function() {
+			console.log("-------------" + sql + " CB");
+			if(cb != null) cb();
+		}, 1000);
+	}
+};
+//Enable queuing
+queue(db);
+
+db.query("1");
+db.query("2", function() {
+	db.query("2cb 1");
+	db.query("2cb 2", function() {
+		db.query("2cb 2cb");
+	});
+});
+db.query("3A");
+
+var q1 = db.createQueue();
+q1.query("Q1 1");
+q1.query("Q1 2", function() {
+	db.query("6", function() {
+		q1.query("9", function() {
+			q1.query("11");
+		});
+		db.query("8");
+		q1.execute();
+		q1.query("10");
+		db.query("12");
+	});
+	q1.query("Q1 2cb 1");
+	q1.query("Q1 2cb 2", function() {
+		q1.query("Q1 2cb 2cb");
+	});
+});
+q1.query("Q1 3");
+db.query("3B");
+q1.execute();
+db.query("4");
+
+//Should do nothing
+q1.execute();
+q1.execute();
+q1.execute();
+
+var q2 = db.createQueue();
+q2.query("Q2 1");
+q2.query("Q2 2", function() {
+	db.query("7");
+	q2.query("Q2 2cb 1");
+	q2.query("Q2 2cb 2", function() {
+		q2.query("Q2 2cb 2cb");
+	});
+});
+q2.query("Q2 3");
+q2.execute();
+db.query("5");
